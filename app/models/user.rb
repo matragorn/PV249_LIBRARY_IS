8818ebbdde0807ba34b05ee6a34b0d4afd12bd06
matus <matus.smykala@gmail.com>
2016-01-23 14:41:37 +0100

    fixed avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+
   has_attached_file :avatar, styles: { thumb: "100x100>" },
                     :default_url => "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120193611) do
+ActiveRecord::Schema.define(version: 20160123121307) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -94,7 +94,10 @@ ActiveRecord::Schema.define(version: 20160120193611) do
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.attachment :avatar
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
